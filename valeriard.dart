6b18410e8504fa12da2825a2fc7a378335dd58fe
valeriard <vriverad@choucairtesting.com>
2022-10-03 15:11:51 -0500

    Solucion reto 1 codeabbey

---
+++ valeriard.dart
@@ -0,0 +1,16 @@
+/*
+  Language: Dart
+  Path: valeriard.dart
+*/
+
+void main (){ 
+var a =1920 ;
+var b =1975 ;
+var suma = a + b;
+print('El resultado de la suma es: $suma');
+}
+
+  /*
+  $ cat DATA.lst | ./valeriard
+  3895
+  */
